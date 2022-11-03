eee773df62c78067159069f4d2ce1d900627be53
atere21 <tosinateret@gmail.com>
2022-11-03 05:38:51 -0600

    form-created

--- src/Contact.js
+++ src/Contact.js
@@ -3,8 +3,16 @@ import './Contact.css'
 
 function Contact() {
   return (
-    <div className='contact'>
-
+    <div className='contact'> 
+        <div className='text'><h2>Contact Me</h2>
+        <p>Hi there, contact me to ask me about anything you have in mind</p></div>
+            <form action='#'>
+                <div className='input-data'>
+                    <input type='text' />
+                    <label>First Name</label>
+                </div>
+            </form>
+            
     </div>
   )
 }
