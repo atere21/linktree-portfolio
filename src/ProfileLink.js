59ecf0a97759c1dd8e1d9b3b4333c5ec5f9ee54b
atere21 <tosinateret@gmail.com>
2022-11-04 20:40:09 -0600

    styling: quick-fix

--- src/ProfileLink.js
+++ src/ProfileLink.js
@@ -19,13 +19,13 @@ function ProfileLink() {
           <h2>Oluwatosin Atere</h2>
           </div >
        
-      <a id='twitter' href='https://twitter.com/flourishgold212' target='_blank'><h3>Twitter Link </h3></a>
-      <a id="btn__zuri" href="https://training.zuri.team/" target="_blank"><h3 id="btn__zuri">Zuri Team </h3></a>
-      <a id="btn__zuri" href="https://books.zuri.team" target="_blank"><h3 id="btn__zuri">Zuri Books </h3></a>
-      <a id="book__python" href="https://books.zuri.team/python-for-beginners?ref_id=<OluwatosinAtere>" target="_blank"><h3>Python Books</h3></a>
-      <a id="pitch" href="https://background.zuri.team" target="_blank"><h3>Background Check for Coders</h3></a>
-      <a href='https://books.zuri.team/design-rules' target="_blank"><h3>Design Books</h3></a>
-      <a id='contact' href='./Contact' target="_blank"><h3>Contact Me</h3></a>
+      <a id='twitter' href='https://twitter.com/flourishgold212'  rel='noreferrer' ><h3>Twitter Link </h3></a>
+      <a id="btn__zuri" href="https://training.zuri.team/" rel='noreferrer' ><h3 id="btn__zuri">Zuri Team </h3></a>
+      <a id="btn__zuri" href="https://books.zuri.team" rel='noreferrer'><h3 id="btn__zuri">Zuri Books </h3></a>
+      <a id="book__python" href="https://books.zuri.team/python-for-beginners?ref_id=<OluwatosinAtere>"  rel='noreferrer'><h3>Python Books</h3></a>
+      <a id="pitch" href="https://background.zuri.team"  rel='noreferrer'><h3>Background Check for Coders</h3></a>
+      <a href='https://books.zuri.team/design-rules'  rel='noreferrer'><h3>Design Books</h3></a>
+      <a id='contact' href='./Contact'  rel='noreferrer'><h3>Contact Me</h3></a>
       </div>
     </div>
   )
